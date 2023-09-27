a5a37706276fa9e50e25c483f50228247c4cb0d7
User <abbosmardiev1999@gmail.com>
2023-09-27 18:40:21 +0500

    Add Pagination

--- app/src/main/java/uz/bismillah/siyrat/MainActivity.kt
+++ app/src/main/java/uz/bismillah/siyrat/MainActivity.kt
@@ -2,10 +2,14 @@ package uz.bismillah.siyrat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import uz.bismillah.siyrat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
