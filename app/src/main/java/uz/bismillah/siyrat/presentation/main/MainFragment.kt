71814a6f489c122145c1e8ed4f72bf52c20ae2c0
User <abbosmardiev1999@gmail.com>
2023-09-28 13:16:30 +0500

    Add Pagination

---
+++ app/src/main/java/uz/bismillah/siyrat/presentation/main/MainFragment.kt
@@ -0,0 +1,11 @@
+package uz.bismillah.siyrat.presentation.main
+
+import uz.bismillah.siyrat.databinding.FragmentMainBinding
+
+class MainFragment : BaseMainFragment<FragmentMainBinding>(FragmentMainBinding::inflate) {
+
+    override fun onViewCreate() {
+
+    }
+
+}
